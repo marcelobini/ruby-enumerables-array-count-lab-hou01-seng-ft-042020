978da11c73dddbea3eb5c1966cde2695211e9bfa
Marcelo Bini <marcelobini@gmail.com>
2020-04-04 19:10:10 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,7 +2,7 @@ def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable\
   index = 0
   while index < array.count do
-    if array [index]is.String?
+    if array[index].str?
 
   end
 
